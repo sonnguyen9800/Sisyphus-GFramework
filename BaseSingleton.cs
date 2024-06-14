16bcf2f2d8a288d6be3ed7ab529d8ba0bbc745d6
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-06-14 07:19:20 +0700

    Update

---
+++ BaseSingleton.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public abstract partial class BaseSingleton<T> : Node where T : Node
+{
+	private static T _instance = null;
+	public static T Instance => _instance;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public override void _EnterTree()
+	{
+		if (_instance != null)
+		{
+			this.QueueFree(); // The Singletone is already loaded, kill this instance
+			GD.PrintErr("This singleton already loaded. Remove this");
+		}
+		_instance = this as T;
+	}
+}

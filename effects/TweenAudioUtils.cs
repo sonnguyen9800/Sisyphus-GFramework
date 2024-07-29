af276031aef3a4ad103fa109b6fe795932e69aef
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-07-29 21:45:22 +0700

    Tween Audio

---
+++ effects/TweenAudioUtils.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisyphusFramework
+{
+    public static class TweenAudioUtils
+    {
+        public enum TweenType
+        {
+            FadeIn,
+            FadeOut,
+        }
+
+        public static async Task TweenSound(AudioStreamPlayer audio, float target, float durationFadeIn = 1.0f, TweenType type = TweenType.FadeIn)
+        {
+            var tween = audio.GetTree().CreateTween();
+            tween.TweenProperty(audio, "volume_db", target, durationFadeIn).SetEase(type == TweenType.FadeIn ? Tween.EaseType.Out : Tween.EaseType.In);
+            await audio.ToSignal(tween, Tween.SignalName.Finished);
+        }
+        public static async Task TweenSound(AudioStreamPlayer2D audio, float target, float durationFadeIn = 1.0f, TweenType type = TweenType.FadeIn)
+        {
+            var tween = audio.GetTree().CreateTween();
+            tween.TweenProperty(audio, "volume_db", target, durationFadeIn).SetEase(type == TweenType.FadeIn ? Tween.EaseType.Out : Tween.EaseType.In);
+            await audio.ToSignal(tween, Tween.SignalName.Finished);
+        }
+    }
+}
